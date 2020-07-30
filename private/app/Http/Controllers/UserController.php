23ad9b408182c2a391a1210c1f66e811f103f509
xiangyu1888 <Ideveloper2020@hotmail.com>
2020-07-30 04:48:58 -0700

    clean subscribe

--- private/app/Http/Controllers/UserController.php
+++ private/app/Http/Controllers/UserController.php
@@ -381,117 +381,56 @@ class UserController extends Controller
 
 
     public function subscribe(Request $request)
-
     {
-
         $data = $request->input();
-
         $validation = \Validator::make($request->all(), ValidationRequest::$forgot_email);
-
         if ($validation->fails()) {
-
-              return  Config::get('message.options.REQ_MAIL');
-
+            return  Config::get('message.options.REQ_MAIL');
         }
-
         $email = $data['email'];
-
-
-
         // MailChimp API credentials
-
         $apiKey = 'b68be37a6121571f7b6881d38742ef7f-us18';
-
         //$apiKey = 'f58126ef0cbfe1c6337a83096fba892c-us3';
-
         $listID = 'c2e6acc490';
-
-
-
         // MailChimp API URL
-
         $memberID = md5(strtolower($email));
-
         $dataCenter = substr($apiKey,strpos($apiKey,'-')+1);
-
         $url = 'https://' . $dataCenter . '.api.mailchimp.com/3.0/lists/' . $listID . '/members/' . $memberID;
-
-
-
         // member information
-
         $json = json_encode([
-
             'email_address' => $email,
-
             'status'        => 'subscribed',
-
             'merge_fields'  => [
-
                 'FNAME'     => ''
-
             ]
-
         ]);
-
-
-
         // send a HTTP POST request with curl
-
         $ch = curl_init($url);
-
         curl_setopt($ch, CURLOPT_USERPWD, 'user:' . $apiKey);
-
         curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
-
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
         curl_setopt($ch, CURLOPT_TIMEOUT, 10);
-
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
-
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-
         curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
-
         $result = curl_exec($ch);
-
         $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-
         curl_close($ch);
-
         //print_r($result);die;
-
-
-
         // store the status message based on response code
-
         if ($httpCode == 200) {
-
             echo 'You have successfully subscribed to our news letter.';
-
         } else {
-
             switch ($httpCode) {
-
                 case 214:
-
                     echo    $msg = 'You are already subscribed.';
-
                     break;
-
                 default:
-
                     echo     $msg = 'Some problem occurred, please try again.';
-
                     break;
-
             }
-
             print_r($msg);
-
         }
-
     }
 
 
